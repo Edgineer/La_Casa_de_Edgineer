c5c61284101f6f64fa1c629410820ed405898bc7
Edgineer <elopez42@g.ucla.edu>
2019-07-02 10:08:49 -0700

    Updates

---
+++ precache-manifest.5315bd9d83dc2550efadb15d205c8e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63326aa98979efc11830a72db8190fa5",
+    "url": "/La-Casa-de-Edgineer/index.html"
+  },
+  {
+    "revision": "c800b45a986c5a6ab764",
+    "url": "/La-Casa-de-Edgineer/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/La-Casa-de-Edgineer/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "c800b45a986c5a6ab764",
+    "url": "/La-Casa-de-Edgineer/static/js/main.55f6c383.chunk.js"
+  },
+  {
+    "revision": "078fc10d17d31c38b2c8",
+    "url": "/La-Casa-de-Edgineer/static/js/runtime~main.cb96b01c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/La-Casa-de-Edgineer/static/media/logo.5d5d9eef.svg"
+  }
+]);
